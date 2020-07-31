621760dacd67cd3505e51816e7c9b068a99fbce6
HappyCodingMama <happycodingmama@gmail.com>
2020-07-31 23:06:33 +0900

    summarizing and coding

--- README.md
+++ README.md
@@ -15,3 +15,59 @@
 * controller module
 - calculate budget
 
+1. define variable budgetController 
+apply secret module pattern - (function() { })();
+ 
+2. variable UIcontroller
+ apply secret module pattern - (function() { })();
+
+3. variable controller -> it's Gloval app controller 
+ apply secret module pattern - (function() { })();
+
+ function properties:budgetCtrl, UICtrl
+ call budgetController, UIController
+
+- event handler setup -> add event to class : 'add_btn' when click
+	
+- event handler setup -> add event when keypress, declare function
+property is event
+- if press keyCode is the same as 13 or which 13, then call function ctrlAddItem 
+
+- define ctrlAdddItem and put it in anonymous function
+- put ctrlAddItem in event handler of 'add_btn' 
+
+4. edit variable UIController
+- write return 
+ input get value of variable type->class add_type, 
+description->class add_description,
+value->class add_value 
+ then return anonymous function
+
+
+-define variable input at function of ctrlAddItem in variable controller
+: assign getInput to the property UICtrl
+
+
+---------------1. Get the field input data -------------
+
+1. at UIController
+
+define variable DOMstrings
+- make object bracket and arrange class syntax
+ (inputType, inputDescription,inputValue,inputBtn)
+
+
+- call getDOMstrings write function method return DOMstrings
+
+
+2. at controller
+
+- define variable DOM : assign getDOMstrings to UICtrl 
+
+- put inputBtn in DOMstrings, then change'add_btn' into DOM.inputBtn
+
+
+
+
+
+
---
+++ app.js
@@ -0,0 +1,78 @@
+/* var budgetController = (function () {
+
+  var x = 23;
+
+  var add = function (a) {
+    return x + a;
+  }
+
+  return {
+    publicTest: function (b) {
+      return add(b);
+    }
+  }
+
+})();
+
+var UIController = (function () {
+
+})();
+
+var controller = (function (budgetCtrl, UICtrl) {
+  var z = budgetCtrl.publicTest(5);
+
+  return {
+    anotherPublic: function () {
+      console.log(z);
+    }
+  }
+})(budgetController, UIController); */
+
+
+
+var budgetController = (function () {
+
+})();
+
+var UIController = (function () {
+
+  var DOMstrings = {
+    inputType: '.add_type',
+    inputDescription: '.add_description',
+    inputValue: '.add_value',
+    inputBtn: '.add_btn'
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDOMstrings: function () {
+      return DOMstrings;
+    }
+  }
+})();
+
+var controller = (function (budgetCtrl, UICtrl) {
+
+  var DOM = UICtrl.getDOMstrings();
+
+  var ctrlAddItem = function () {
+    var input = UICtrl.getInput();
+    console.log(input);
+  }
+
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
+
+  document.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+
+})(budgetController, UIController);
